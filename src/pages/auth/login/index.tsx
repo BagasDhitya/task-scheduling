38bada95d666973a7eb82c42c182592537673781
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-08-02 15:50:12 +0700

    add: profile photo handler

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { setUser, setToken } from "../../../utils/redux/authSlice";
-import Layout from "../../../components/Layout";
-import Input from "../../../components/Input";
-import Button from "../../../components/Button";
+import { setUser, setToken } from "@/utils/redux/authSlice";
+import Layout from "@/components/Layout";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
 import { useRouter } from "next/router";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value,
+    }));
+  };
+
   const handleLogin = () => {
-    const user = { username, password };
-    const token = "dummy-token";
-    dispatch(setUser(user));
-    dispatch(setToken(token));
-    router.push("/dashboard");
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      if (
+        user.username === credentials.username &&
+        user.password === credentials.password
+      ) {
+        const token = "dummy-token";
+        dispatch(setUser(user));
+        dispatch(setToken(token));
+        router.push("/dashboard");
+      } else {
+        alert("Invalid username or password");
+      }
+    } else {
+      alert("No user found. Please register first.");
+    }
   };
 
   return (
@@ -25,14 +47,16 @@ const Login = () => {
       <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
         <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
         <Input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
           placeholder="Username"
         />
         <Input
+          name="password"
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={credentials.password}
+          onChange={handleChange}
           placeholder="Password"
         />
         <Button onClick={handleLogin} className="w-full">
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,19 +1,52 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import { useDispatch } from "react-redux";
-import { setUser, setToken } from "../../../utils/redux/authSlice";
-import Layout from "../../../components/Layout";
-import Input from "../../../components/Input";
-import Button from "../../../components/Button";
+import { setUser, setToken } from "@/utils/redux/authSlice";
+import Layout from "@/components/Layout";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import { User } from "@/utils/interface/types";
 import { useRouter } from "next/router";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState<User>({
+    username: "",
+    email: "",
+    password: "",
+    profilePhoto: "",
+  });
+
+  const [previewPhoto, setPreviewPhoto] = useState<string>("");
+
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFormData((prevData) => ({
+          ...prevData,
+          profilePhoto: reader.result as string,
+        }));
+        setPreviewPhoto(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleRegister = () => {
-    const user = { username, password };
+    const { username, email, password, profilePhoto } = formData;
+    const user = { username, email, password, profilePhoto };
+    console.log(user);
     const token = "dummy-token";
     dispatch(setUser(user));
     dispatch(setToken(token));
@@ -24,15 +57,54 @@ const Register = () => {
     <Layout isHidden={true}>
       <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
         <h2 className="text-2xl text-center font-bold mb-4">Register</h2>
+        <div className="flex flex-col items-center mb-4 relative">
+          <div className="w-24 h-24 mb-4 rounded-full bg-gray-200 overflow-hidden">
+            {previewPhoto ? (
+              <img
+                src={previewPhoto}
+                alt="Profile"
+                className="object-cover w-full h-full"
+              />
+            ) : (
+              <img
+                src="https://hwchamber.co.uk/wp-content/uploads/2022/04/avatar-placeholder.gif"
+                alt="Placeholder"
+                className="object-cover w-full h-full"
+              />
+            )}
+          </div>
+          <button
+            className="absolute bottom-0 right-40 w-8 h-8 bg-emerald-500 text-white rounded-full flex items-center justify-center"
+            onClick={() => document.getElementById("file-input")?.click()}
+          >
+            +
+          </button>
+          <input
+            id="file-input"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+        </div>
         <Input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
           placeholder="Username"
         />
         <Input
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <Input
+          name="password"
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formData.password}
+          onChange={handleChange}
           placeholder="Password"
         />
         <Button onClick={handleRegister} className="w-full">
