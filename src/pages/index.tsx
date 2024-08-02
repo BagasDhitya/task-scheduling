516c3e583c3520f492b56485a531fbf421b9d99c
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-08-02 15:57:35 +0700

    fix: migrate dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,141 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, updateTask, deleteTask } from "@/utils/redux/taskSlice";
+import Layout from "@/components/Layout";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import { Task } from "@/utils/interface/types";
+import { RootState } from "@/utils/redux/store";
+import Modal from "@/components/Modal";
+import Loading from "@/components/Loading";
 
-const inter = Inter({ subsets: ["latin"] });
+const Dashboard = () => {
+  const [task, setTask] = useState<Task>({ id: 0, title: "", description: "" });
+  const [editTask, setEditTask] = useState<Task | null>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const [loading, setLoading] = useState<{ status: boolean; message: string }>({
+    status: false,
+    message: "",
+  });
+  const tasks = useSelector((state: RootState) => state.tasks.tasks);
+  const dispatch = useDispatch();
 
-export default function Home() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsMounted(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleAddTask = async () => {
+    setLoading({ status: true, message: "Adding Task..." });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    dispatch(addTask({ ...task, id: Date.now() }));
+    setTask({ id: 0, title: "", description: "" });
+    setLoading({ status: false, message: "" });
+  };
+
+  const handleUpdateTask = async (task: Task) => {
+    setLoading({ status: true, message: "Updating Task..." });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    dispatch(updateTask(task));
+    setShowModal(false);
+    setLoading({ status: false, message: "" });
+  };
+
+  const handleDeleteTask = async (id: number) => {
+    setLoading({ status: true, message: "Deleting Task..." });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    dispatch(deleteTask(id));
+    setLoading({ status: false, message: "" });
+  };
+
+  const openEditModal = (task: Task) => {
+    setEditTask(task);
+    setShowModal(true);
+  };
+
+  if (!isMounted || loading.status) {
+    return (
+      <Loading message={loading.message ? loading.message : "Loading..."} />
+    );
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <ProtectedRoute>
+      <Layout>
+        <div className="max-w-md my-20 mx-auto bg-white p-6 rounded-md shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
+          <Input
+            value={task.title}
+            onChange={(e) => setTask({ ...task, title: e.target.value })}
+            placeholder="Title"
+          />
+          <Input
+            value={task.description}
+            onChange={(e) => setTask({ ...task, description: e.target.value })}
+            placeholder="Description"
+          />
+          <Button onClick={handleAddTask} className="w-full">
+            Add Task
+          </Button>
+
+          <ul className="mt-4">
+            {tasks.map((task) => (
+              <li key={task.id} className="bg-gray-100 p-2 mb-2 rounded">
+                <h3 className="text-xl font-bold">{task.title}</h3>
+                <p>{task.description}</p>
+                <Button
+                  onClick={() => openEditModal(task)}
+                  className="bg-yellow-500 text-white p-1 rounded mr-2"
+                >
+                  Edit
+                </Button>
+                <Button
+                  onClick={() => handleDeleteTask(task.id)}
+                  className="bg-red-500 text-white p-1 rounded"
+                >
+                  Delete
+                </Button>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {editTask && (
+          <Modal
+            show={showModal}
+            onClose={() => setShowModal(false)}
+            title="Edit Task"
+          >
+            <Input
+              value={editTask.title}
+              onChange={(e) =>
+                setEditTask({ ...editTask, title: e.target.value })
+              }
+              placeholder="Title"
+            />
+            <Input
+              value={editTask.description}
+              onChange={(e) =>
+                setEditTask({ ...editTask, description: e.target.value })
+              }
+              placeholder="Description"
+            />
+            <Button
+              onClick={() => handleUpdateTask(editTask)}
+              className="bg-blue-500 text-white p-1 rounded w-full"
+            >
+              Update Task
+            </Button>
+          </Modal>
+        )}
+      </Layout>
+    </ProtectedRoute>
   );
-}
+};
+
+export default Dashboard;
